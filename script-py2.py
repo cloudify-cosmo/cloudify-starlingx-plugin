ea610831e59f6a8f5f314842f8a13a0c18b32974
earthmant <trammell@cloudify.co>
2021-02-17 10:34:52 +0200

    add labels to blueprint

---
+++ script-py2.py
@@ -0,0 +1,224 @@
+
+import os
+import sys
+import json
+import logging
+
+from dcmanagerclient.api import client
+from cgtsclient.client import get_client
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+client_args = dict(
+    auth_url=os.environ['OS_AUTH_URL'],
+    username=os.environ['OS_USERNAME'],
+    api_key=os.environ['OS_PASSWORD'],
+    project_name=os.environ['OS_PROJECT_NAME'],
+    # user_domain_name=os.environ['OS_USER_DOMAIN_NAME'],
+    # project_domain_name=os.environ['OS_PROJECT_NAME'],
+    # project_domain_id=os.environ['OS_PROJECT_DOMAIN_ID']
+)
+
+dc_client_args = dict(
+    auth_url=os.environ['OS_AUTH_URL'],
+    username=os.environ['OS_USERNAME'],
+    api_key=os.environ['OS_PASSWORD'],
+    project_name=os.environ['OS_PROJECT_NAME'],
+    user_domain_name=os.environ['OS_USER_DOMAIN_NAME'],
+    project_domain_name=os.environ['OS_PROJECT_NAME'],
+    project_domain_id=os.environ['OS_PROJECT_DOMAIN_ID']
+)
+
+os_client_args = {}
+for key, val in client_args.items():
+    os_client_args['os_{key}'.format(key=key)] = val
+os_client_args['os_password'] = os_client_args.pop('os_api_key')
+os_client_args['os_region_name'] = 'RegionOne'
+os_client_args['api_version'] = 1
+config_client = get_client(**os_client_args)
+dc_client = client.client(**dc_client_args)
+
+
+class System(object):
+
+    def __init__(self, api_response):
+        self.api_response = api_response
+        self.id = self.api_response.uuid
+        self.name = self.api_response.name
+
+        try:
+            self.distributed_cloud_role = \
+                self.api_response.distributed_cloud_role.lower()
+        except AttributeError:
+            self.distributed_cloud_role = \
+                self.api_response.distributed_cloud_role
+
+        self.system_type = self.api_response.system_type.lower()
+        self.system_mode = self.api_response.system_mode.lower()
+
+    @property
+    def is_system_controller(self):
+        return self.distributed_cloud_role == 'systemcontroller'
+
+    @property
+    def is_subcloud(self):
+        return self.distributed_cloud_role == 'subcloud'
+
+    @property
+    def is_standalone_system(self):
+        # It is not clear how this will appear in the API response.
+        if self.distributed_cloud_role == 'null':
+            return True
+        elif self.api_response.distributed_cloud_role is None:
+            return True
+        return False
+
+    @property
+    def is_all_in_one(self):
+        return self.system_type == 'all-in-one'
+
+    @property
+    def is_standard(self):
+        # Not clear how this value will appear in API response.
+        return self.system_type == 'standard'
+
+    @property
+    def is_duplex(self):
+        return self.system_mode == 'duplex'
+
+    @property
+    def is_simplex(self):
+        # Not clear how this value will appear in API response.
+        return self.system_mode == 'simplex'
+
+    @property
+    def __str__(self):
+        return self.api_response.__dict__
+
+
+def get_systems():
+    return [System(system) for system in config_client.isystem.list()]
+
+
+def get_subclouds():
+    subclouds = []
+    for subcloud in dc_client.subcloud_manager.list_subclouds():
+        subcloud_detail = \
+            dc_client.subcloud_manager.subcloud_additional_details(
+                subcloud.name)[0]
+        subclouds.append(subcloud_detail)
+    return subclouds
+
+
+def get_hosts():
+    return config_client.ihost.list()
+
+
+def print_hosts_for_system(system_id):
+    for host in get_hosts():
+        if host.isystem_uuid == system_id:
+            print_host(host)
+
+
+def print_subclouds_for_system(system_name):
+    logging.error(
+        "We do not know how to identify the subclouds " 
+        "for system {name}. What is the criteria, " 
+        "when I look at the list of subclouds, " 
+        "to deduce which are managed by this system controller?".format(
+            name=system_name))
+    # If I just list all subclouds, some of them will be for
+    # certain System Controllers, and others will belong to others.
+    # How do I identify a subcloud as being managed by a
+    # specific system controller?
+    # I thought I had a solution, but it does not work.
+
+
+def print_subcloud_system_controller(system_name):
+    logging.error(
+        "We do not know how to identify the system controller " 
+        "for subcloud {name}. What is the criteria, " 
+        "when I look at the list of systems, "
+        "to deduce which are managing this subcloud?".format(
+            name=system_name))
+    # How can I discover the System Controller that manages a specific
+    # subcloud?
+
+
+def print_system(system):
+    logging.info(system.__dict__)
+
+
+def print_host(host):
+    logging.info(host.to_dict())
+
+
+def print_subcloud(subcloud):
+    logging.info(subcloud.__dict__)
+
+
+def show_system_info():
+
+    logging.info("Showing info for systems in environment.")
+
+    for system in get_systems():
+        # I noticed that in the CLI `system show`, we can only show one system
+        # controller.
+        # In the Python client, there is no such limitation, but it makes me
+        # suspect, that one may only ever see one System Controller a single
+        # credential.
+
+        if system.is_duplex:
+            logging.info("The system is duplex with hosts:")
+            print_hosts_for_system(system.id)
+
+        elif system.is_simplex:
+            logging.info("The system is simplex and will have one host.")
+            print_hosts_for_system(system.id)
+
+        else:
+            logging.info("The system is {mode}.".format(
+                mode=system.api_response.system_mode))
+
+        if system.is_standalone_system:
+            logging.info(
+                "The system is a " 
+                "Standalone system and will have no subclouds.")
+            print(system)
+
+        elif system.is_system_controller:
+            logging.info(
+                "The system is a " 
+                "System Controller and has the following subclouds.")
+            print_subclouds_for_system(system.name)
+
+        elif system.is_subcloud:
+            logging.info(
+                "The system is a " 
+                "Subcloud, it is managed by this System Controller:")
+            print_subcloud_system_controller(system.name)
+            # Let's say that we have subclouds,
+            # which are also system controllers,
+            # how can we find subclouds of subclouds?
+
+
+def show_diagnostics():
+
+    logging.info("Diagnostics: ")
+
+    logging.info("These are the systems available: ")
+    for system in get_systems():
+        print_system(system)
+
+    logging.info("These are the hosts available: ")
+    for host in get_hosts():
+        print_host(host)
+
+    logging.info("These are the subclouds available: ")
+    for subcloud in get_subclouds():
+        print_subcloud(subcloud)
+
+
+if __name__ == '__main__':
+    show_system_info()
+    show_diagnostics()
--- script-py3.py
+++ script-py3.py
@@ -1,14 +1,8 @@
-# For the exercises here, set up the following environment.
 
 import os
 import json
-from copy import deepcopy
-
 from dcmanagerclient.api import client
 from cgtsclient.client import get_client
-
-# Collect client configurations:
-
 client_args = dict(
     auth_url=os.environ['OS_AUTH_URL'],
     username=os.environ['OS_USERNAME'],
@@ -28,18 +22,14 @@ dc_client_args = dict(
     project_domain_name=os.environ['OS_PROJECT_NAME'],
     project_domain_id=os.environ['OS_PROJECT_DOMAIN_ID']
 )
-
 os_client_args = {}
 for key, val in client_args.items():
     os_client_args['os_{key}'.format(key=key)] = val
 os_client_args['os_password'] = os_client_args.pop('os_api_key')
+os_client_args['os_region_name'] = 'RegionOne'
 os_client_args['api_version'] = 1
-
 config_client = get_client(**os_client_args)
 dc_client = client.client(**dc_client_args)
-
-# Method for clean printing:
-
 
 def print_object(obj):
     if hasattr(obj, 'to_dict'):
@@ -50,22 +40,39 @@ def print_object(obj):
         print('Error: no way to dump obj...')
         print(json.dumps(obj, indent=4, sort_keys=True))
 
-# I can list deployment configurations:
-# These are either: "all-in-one simplex", "all-in-one duplex", etc.
-# In our lab, we only have "all-in-one duplex".
-# My assumption is that these are called
+def print_something(something):
+    c = getattr(config_client, something)
+    for x in c.list():
+        print_object(x)
+
+def print_systems():
+    print('Displaying Systems')
+    for system in config_client.isystem.list():
+        print_object(system)
+
+def print_hosts():
+    print('Displaying Hosts\n')
+    for host in config_client.ihost.list():
+        print_object(host)
+
+def print_apps():
+    for app in config_client.app.list():
+        print(json.dumps(app, indent=4, sort_keys=True))
+
+def print_subclouds():
+    print('Displaying Subclouds')
+    for subcloud in dc_client.subcloud_manager.list_subclouds():
+        subcloud_dict = subcloud.__dict__
+        sc = dc_client.subcloud_manager.subcloud_additional_details(subcloud.name)
+        delattr(sc[0], 'manager')
+        print(json.dumps(sc[0].__dict__, indent=4, sort_keys=True))
+
+def print_subcloud_groups():
+    for group in dc_client.subcloud_group_manager.list_subcloud_groups():
+        print(json.dumps(group, indent=4, sort_keys=True))
 
 
-def print_isystems():
-    print('Displaying System Info')
-    for isystem in config_client.isystem.list():
-        print_object(isystem)
-        for ihost in config_client.ihost.list():
-            if ihost.isystem_uuid != isystem.uuid:
-                continue
-            print_object(ihost)
-        internal_dc_client_args = deepcopy(dc_client_args)
-        internal_dc_client_args['region_name'] = isystem.region_name
-        internal_dc_client = client.client(**internal_dc_client_args)
-        for subcloud in internal_dc_client.subcloud_manager.list_subclouds():
-            print_object(subcloud)
+  # System Type All-in-one/Simplex, All-in-one/Duplex, standard - physical topology
+  # System Mode Simplex Duplex - relationship between the two controller hosts
+  # Distributed Cloud Role - Null - standalond - no discover , or non-null get a subcloud list
+
