c674e5c51524185226354884c9dd46cac828dce8
Mateusz Mizerski <106737627+mateuszmizer@users.noreply.github.com>
2022-10-27 10:49:08 +0200

    Update upgrade.py

--- cloudify_starlingx/resources/upgrade.py
+++ cloudify_starlingx/resources/upgrade.py
@@ -12,7 +12,7 @@ from cloudify_starlingx_sdk.resources.configuration import SystemResource
 
 
 @with_starlingx_resource(SystemResource)
-def upgrade(resource, sw_version=None, license_file_path='', ctx=None, **_):
+def upgrade(resource, sw_version=None, license_file_path='', iso_path='', sig_path='', ctx=None, **_):
     client_config = resource.client_config
     auth_url = client_config.get('auth_url')
     username = client_config.get('username')
@@ -21,57 +21,17 @@ def upgrade(resource, sw_version=None, license_file_path='', ctx=None, **_):
     user_domain_id = client_config.get('user_domain_id')
     project_domain_id = client_config.get('project_domain_id')
 
+    host_runtime_properties = ctx.instance.runtime_properties.get('hosts', {})
+    controlers_list = [v["hostname"] for k, v in host_runtime_properties.items() if v["subfunctions"] == "controller"]
+    workers_list = [v["hostname"] for k, v in host_runtime_properties.items() if v["subfunctions"] == "worker"]
+
     upgrade_client = UpgradeClient.get_upgrade_client(auth_url=auth_url, username=username , password=password,
                                                       project_name=project_name, user_domain_id=user_domain_id,
                                                       project_domain_id=project_domain_id)
 
     # Upgrade steps
 
-    # Make sure we are connected to controller-0
 
-    # 1. Install license for new release
-    upgrade_client.apply_license(license_file_path=license_file_path)
-
-    # 2. Upload ISO and SIG files to controller-0
-    upgrade_client.upload_iso_and_sig_files(iso_path='', sig_path='', active='true', local='true')
-
-    # 3. Run upgrade start
-    upgrade_client.do_upgrade_start(force=True)
-
-    # 4. Verify that upgrade has started
-    upgrade_client.do_upgrade_show()
-
-    # 5. Lock controller-1
-    upgrade_client.do_host_lock(hostname_or_id='', force=True)
-
-    # 6. Upgrade controller-1
-    upgrade_client.do_host_upgrade(host_id='', force=True)
-
-    # 7. Check upgrade status
-    upgrade_client.do_upgrade_show()
-
-    # 8. Unlock controller-1
-    upgrade_client.do_host_unlock(hostname_or_id='', force=True)
-
-    # 9. Wait for controller-1 to become unlocked-enabled
-    upgrade_client.do_host_show(hostname_or_id='', column='', format='')
-
-    # 10. TBD: Wait for the DRBD sync 400.001 Services-related alarm is raised and then cleared
-
-    # 11. Set controller-1 as active controller
-    upgrade_client.do_host_swact(hostname_or_id='', force=True)
-
-    # 12. Wait for all services on controller-1 are enabled-active, the swact is complete
-    upgrade_client.wait_for_swact()
-
-    # 13. Lock controller-0
-    upgrade_client.do_host_swact(hostname_or_id='', force=True)
-
-    # 14. Upgrade controller-0
-    upgrade_client.do_host_upgrade(host_id='', force=True)
-
-    # 15. Unlock Controller-0
-    upgrade_client.do_host_unlock(hostname_or_id='', force=True)
 
     # 16. Upgrde ceph sotrage (if in use) - repeat for each storage node
 
@@ -143,3 +103,50 @@ def upgrade(resource, sw_version=None, license_file_path='', ctx=None, **_):
     # 23.4 Delete upgrade strategy
     dc_patch_client.delete_update_strategy(type_of_strategy='upgrade')
 
+
+def _upgrade_controlers(upgrade_client, controlers_list, license_file_path, iso_path, sig_path):
+        # Make sure we are connected to controller-0
+
+    # 1. Install license for new release
+    upgrade_client.apply_license(license_file_path=license_file_path)
+
+    # 2. Upload ISO and SIG files to controller-0
+    upgrade_client.upload_iso_and_sig_files(iso_path=iso_path, sig_path=sig_path, active='true', local='true')
+
+    # 3. Run upgrade start
+    upgrade_client.do_upgrade_start(force=True)
+
+    # 4. Verify that upgrade has started
+    upgrade_client.do_upgrade_show()
+
+    # 5. Lock controller-1
+    upgrade_client.do_host_lock(hostname_or_id='', force=True)
+
+    # 6. Upgrade controller-1
+    upgrade_client.do_host_upgrade(host_id='', force=True)
+
+    # 7. Check upgrade status
+    upgrade_client.do_upgrade_show()
+
+    # 8. Unlock controller-1
+    upgrade_client.do_host_unlock(hostname_or_id='', force=True)
+
+    # 9. Wait for controller-1 to become unlocked-enabled
+    upgrade_client.do_host_show(hostname_or_id='', column='', format='')
+
+    # 10. TBD: Wait for the DRBD sync 400.001 Services-related alarm is raised and then cleared
+
+    # 11. Set controller-1 as active controller
+    upgrade_client.do_host_swact(hostname_or_id='', force=True)
+
+    # 12. Wait for all services on controller-1 are enabled-active, the swact is complete
+    upgrade_client.wait_for_swact()
+
+    # 13. Lock controller-0
+    upgrade_client.do_host_lock(hostname_or_id='', force=True)
+
+    # 14. Upgrade controller-0
+    upgrade_client.do_host_upgrade(host_id='', force=True)
+
+    # 15. Unlock Controller-0
+    upgrade_client.do_host_unlock(hostname_or_id='', force=True)
